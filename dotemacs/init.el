5d642040f9666ec51866a17cd2a7406dabe404cb
Michishige Kaito <me@mkaito.com>
2013-04-29 14:54:48 +0100

    Deleted obsolete dotemacs vendor

--- dotemacs/init.el
+++ dotemacs/init.el
@@ -1,54 +1,38 @@
-1;3404;0c1;3404;0c;; mkaito's dot-emacs on Arch Linux.
+;; mkaito's dot-emacs on Arch Linux.
 ;;
 ;; Large chunks shamelessly copied from anrxc's dotemacs:
 ;;	 http://git.sysphere.org/dotfiles/tree/emacs
 ;; My Emacs wouldn't be half as pleasant without his help.
 ;;
-;; Latest modification: May 15 2011.
+(require 'cl)
 
-;; Actually, what do I want from Emacs?
-;;
-;; * Syntax highlighting for: Markdown, Ruby, JavaScript, Lua, HAML,
-;;	 SASS/SCSS/CSS, HTML.
-;; * Code folding with markers (I don't like the automatic ones)
-;; * Clean interface, low contrast colour theme.
-;; * Snippet expansion
-;;
-;; Organization:
-;;
-;; init.el should contain all common configuration. One file per major
-;; mode, containing all and any configuration relating to this mode,
-;; and one file per major configuration topic if necesary. Avoid
-;; having 250 config files.
-;;
-
-(setq load-path
-      (append
-       (list
-	"/usr/share/emacs/site-lisp/icicles"
-	"/usr/share/emacs/site-lisp/"
-	"~/.emacs.d"
-	"~/.emacs.d/vendor"
-	;; "~/.emacs.d/vendor/color-theme-ir-black"
-	;;"~/.emacs.d/vendor/haml-mode"
-	;;"~/.emacs.d/vendor/sass-mode"
-	;;"~/.emacs.d/vendor/coffee-mode"
-	;; "~/.emacs.d/vendor/smart-tabs-mode"
-	"~/.emacs.d/vendor/nginx-mode"
-	;; "~/.emacs.d/vendor/yasnippet"
-	;; "~/.emacs.d/vendor/solarized"
-	"/usr/share/ghc-mod-1.11.2/"
-	)
-       load-path))
-
-(require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
-;; Check for stale .elc on startup. Slow, but I only restart Emacs after reboots.
-(byte-recompile-directory "~/.emacs.d/vendor" 0)
+(defvar wanted-packages
+  '(coffee-mode expand-region smart-tabs-mode php-mode
+		gist haml-mode haskell-mode inf-ruby lua-mode
+		markdown-mode paredit projectile js2-mode
+		sass-mode rainbow-mode scss-mode ack-and-a-half
+		yaml-mode zenburn-theme nginx-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(defun wanted-packages-installed-p ()
+  (loop for p in wanted-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (wanted-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p wanted-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
 
 ;; Clean initial buffers, windows.
 (defun buffer-exists (bufname) (not (eq nil (get-buffer bufname)))) 
@@ -61,35 +45,34 @@
 (scroll-bar-mode -1)
 (setq visible-bell t)
 
+;;Auto save and backups
+(setq
+backup-by-copying t
+delete-old-versions t
+kept-new-versions 6
+kept-old-versions 2
+version-control t)
+
+(setq backup-directory-alist
+			`((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+			`((".*" ,temporary-file-directory t)))
+
 ;; Fix dead keys
 (load-library "iso-transl")
 
-;; Fix clipboard
-(global-set-key "\C-w" 'clipboard-kill-region)
-(global-set-key "\M-w" 'clipboard-kill-ring-save)
-(global-set-key "\C-y" 'clipboard-yank)
-
-;; Icicles
-(require 'icicles)
-(eval-after-load "ring" '(progn (require 'ring+)))
-
 ;; Colour theme initialization
-;;	 - http://emacswiki.org/cgi-bin/wiki/ColorTheme
-;(require 'color-theme)
-;(setq color-theme-is-global t)
-;(color-theme-initialize)
-(require 'color-theme-zenburn)
-(color-theme-zenburn)
+(load-theme 'zenburn t)
 
 (global-hl-line-mode 1)
-;(set-face-background 'hl-line "#353535")
+(set-face-background 'hl-line "#353535")
 
 ;; Support 256 colours in screen
 ;;	 - http://article.gmane.org/gmane.emacs.devel/109504/
 (if (not (window-system)) (load "term/rxvt"))
 (defun terminal-init-screen ()
   "Terminal initialization function for screen."
-  ;; Use the rxvt color initialization code.
+  Use the rxvt color initialization code.
   (rxvt-register-default-colors)
   (tty-set-up-initial-frame-faces))
 
@@ -112,45 +95,25 @@
 ;;	 - somewhat cleaner than default
 (setq mode-line-format
       '("-"
-	mode-line-mule-info
-	mode-line-modified
-	mode-line-frame-identification
-	mode-line-buffer-identification
-	"	 "
-	global-mode-string
-	"		%[(" mode-name mode-line-process minor-mode-alist "%n"")%]--"
-	(line-number-mode "L%l--")
-	(column-number-mode "C%c--")
-	(-3 . "%p")
-	"-%-")
+       	mode-line-mule-info
+       	mode-line-modified
+       	mode-line-frame-identification
+       	mode-line-buffer-identification
+       	"	 "
+       	global-mode-string
+       	"		%[(" mode-name mode-line-process minor-mode-alist "%n"")%]--"
+       	(line-number-mode "L%l--")
+       	(column-number-mode "C%c--")
+       	(-3 . "%p")
+       	"-%-")
       )
-
-;; Syntax coloring (font-lock-mode)
-(global-font-lock-mode t)
 
 ;; Always flash for parens and define a more distinctive color
 (show-paren-mode 1)
-;; (set-face-foreground 'show-paren-match-face "#bc8383")
+(set-face-foreground 'show-paren-match-face "#bc8383")
 
 ;; Answer y or n instead of yes or no at prompts
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Use ANSI colors within shell-mode
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-;; Mumamo chunks looks weird...
-(setq mumamo-chunk-coloring 1)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) nil)))
- '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background dark)) nil)))
- '(mumamo-background-chunk-submode2 ((((class color) (min-colors 88) (background dark)) nil)))
- '(mumamo-background-chunk-submode3 ((((class color) (min-colors 88) (background dark)) nil)))
- '(mumamo-background-chunk-submode4 ((((class color) (min-colors 88) (background dark)) nil))))
 
 ;; Set executable bit on scripts after saving
 (add-hook 'after-save-hook
@@ -163,50 +126,34 @@
       ispell-list-command "list"
       ispell-extra-args '("--sug-mode=fast")
       ispell-dictionary "british-ize")
-
+      
 (require 'flyspell)
 (setq flyspell-issue-message-flag nil)
 (global-set-key (kbd "\C-c C-4") 'flyspell-correct-word-before-point)
 (global-set-key (kbd "\C-c C-\\") 'flyspell-correct-word-before-point)
 
-;; Move tempfiles and auto saves elsewhere
-;; (defvar user-temporary-file-directory
-;;   (concat temporary-file-directory user-login-name "/"))
-;; (make-directory user-temporary-file-directory t)
-;; (setq backup-by-copying t)
-;; (setq backup-directory-alist
-;; 	 `(("." . ,user-temporary-file-directory)
-;; 	(,tramp-file-name-regexp nil)))
-;; (setq auto-save-list-file-prefix
-;; 	 (concat user-temporary-file-directory ".auto-saves-"))
-;; (setq auto-save-file-name-transforms
-;;	 `((".*" ,user-temporary-file-directory t)))
-
-;; Auto save options.
-(setq auto-save-visited-file-name t
-      auto-save-interval 300
-      auto-save-timeout 3)
+(global-set-key (kbd "C-=") 'er/expand-region)
 
 ;; Split window navigation: S-arrow
 (windmove-default-keybindings)
 (setq windmove-wrap-around t)
 
-(require 'epa-file)
+;; Encrypted file editing
 (epa-file-enable)
 
+;; Fuzzy matching and all that jazz
 (require 'ido)
 (ido-mode t)
 (setq ido-enable-flex-matching t) ;; enable fuzzy matching
-
 (defadvice completing-read
   (around foo activate)
   (if (boundp 'ido-cur-list)
       ad-do-it
     (setq ad-return-value
-	  (ido-completing-read
-	   prompt
-	   (all-completions "" collection predicate)
-	   nil require-match initial-input hist def))))
+ 	  (ido-completing-read
+ 	   prompt
+ 	   (all-completions "" collection predicate)
+ 	   nil require-match initial-input hist def))))
 
 (global-set-key
  "\M-x"
@@ -218,71 +165,16 @@
       "M-x "
       (all-completions "" obarray 'commandp))))))
 
+(projectile-global-mode)
+(setq projectile-use-native-indexing t)
+(setq projectile-enable-caching t)
+;; ;;(recentf-mode 1)
 
-;; (require 'find-file-in-project)
-;; (setq ffip-find-options "-not -regex \".*vendor.*\"")
-
-;; ;; Assume default-directory if no project root could be found
-;; (setq ffip-project-root-function '(lambda ()
-;; 				    (or
-;; 				     (locate-dominating-file default-directory ffip-project-file)
-;; 				     default-directory)))
-
-;; (setq ffip-patterns
-;;       (append
-;;        (list
-;; 	"Gemfile"
-;; 	"Procfile"
-;; 	"Rakefile"
-;; 	"Makefile"
-;; 	"*.gpg"
-;; 	"*.org"
-;; 	"config.ru"
-;; 	"*.yaml"
-;; 	"*.yml"
-;; 	"*.watchr"
-;; 	"*.rake"
-;; 	"*.gemspec"
-;; 	"*.hrl"
-;; 	"*.mustache"
-;; 	"*.sass"
-;; 	"*.haml"
-;; 	"*.coffee"
-;; 	"*.scss"
-;; 	"*.hs"
-;; 	)
-;;        ffip-patterns))
-
-;; (global-set-key (kbd "C-x f") 'find-file-in-project)
-
-;; Modes
-
-(autoload 'ledger-mode "ledger.el" "Ledger mode." t)
-
-(autoload 'php-mode "php-mode.el" "Php mode." t)
 (setq auto-mode-alist (append '(("/*.\.php[345]?$" . php-mode)) auto-mode-alist))
 
-(autoload 'ghc-init "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode)))
-
-(autoload 'python-mode "python-mode.el" "Python mode." t)
 (setq auto-mode-alist (append '(("/*.\.py$" . python-mode)) auto-mode-alist))
 
-;; Mustache
-;; (require 'mustache-mode)
-(autoload 'mustache-mode "mustache-mode" "Mustache mode" t)
-(add-to-list 'auto-mode-alist '("\\.mustache$" . mustache-mode))
-
-;; Erlang Mode
-;; (setq erlang-root-dir "/usr/lib/erlang")
-;; (setq exec-path (cons "/usr/lib/erlang/bin" exec-path))
-;; (require 'erlang-start)
-
-;; nginx mode
-;; Not going to associate it, just activate with file variables.
-(autoload 'nginx-mode "nginx-mode" "A major mode for editing nginx config files" t)
-
-;; SH mode
+;; ;; SH mode
 (add-hook 'sh-mode-hook
 	  '(lambda ()
 	     (local-set-key (kbd "C-c C-c") '(lambda ()
@@ -291,43 +183,37 @@
 					       (shell-command-on-region (point-min) (point-max) "zsh -vx")))))
 
 ;; Coffee Mode
-(autoload 'coffee-mode "coffee-mode" "Coffee mode" t)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
 (defun coffee-custom ()
   "coffee-mode-hook"
   (set (make-local-variable 'tab-width) 2))
-
 (add-hook 'coffee-mode-hook '(lambda() (coffee-custom)))
 
 ;; Haskell mode
-(load "/usr/share/emacs/site-lisp/haskell-mode/haskell-site-file")
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(load "/usr/share/emacs/site-lisp/haskell-mode/haskell-site-file")
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
 ;; YAML mode
-(autoload 'yaml-mode "yaml-mode" "YAML mode" t)
 (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-mode))
 
 ;; Markdown mode
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'\\|\\.mkd\\'" . markdown-mode))
 (add-hook 'markdown-mode-hook 'visual-line-mode)
 (add-hook 'markdown-mode-hook 'flyspell-mode)
 
 ;; Lua mode for awesome wm config files
-(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-mode))
 
-(load-library "mkaito-org")
+(load "~/.emacs.d/settings-org-mode.el")
 
 ;; Post Mode
-;;		- http://post-mode.sourceforge.net/
-(autoload 'post-mode "post" "Major mode for editing e-mail and journal articles" t)
 (add-to-list 'auto-mode-alist
-	     '("\\.*mutt-*\\|\\.*pico.*\\|\\.*200\\(T\\)?\\|\\.followup" . post-mode))
+	     '("\\.*mutt-*\\|\\.*pico.*\\|\\.*200\\(T\\)?\\|\\.followup" . mail-mode))
+(add-hook 'mail-mode-hook 'auto-fill-mode)
+(add-hook 'mail-mode-hook 'flyspell-mode)
 
 ;; Custom modes for some custom files
 ;; Shell script mode for Arch PKGBUILDs
@@ -336,7 +222,6 @@
 ;; Conf mode for personal config files
 (when (locate-library "conf-mode")
   (autoload 'conf-mode "conf-mode" "Major-mode for editing config files." t)
-  (add-to-list 'auto-mode-alist '("\\awesomerc$" . conf-mode))
   (add-to-list 'auto-mode-alist '("\\gitconfig$" . conf-mode))
   (add-to-list 'auto-mode-alist '("\\screenrc$"	 . conf-mode))
   (add-to-list 'auto-mode-alist '("\\pinerc$"	 . conf-mode))
@@ -352,66 +237,29 @@
 
 ;; Ruby mode hooks
 (add-hook 'ruby-mode-hook 'flyspell-prog-mode)
-(add-hook 'ruby-mode-hook
-	  '(lambda ()
-	     (setq ruby-indent-level 2)
-	     (setq tab-width 2)))
-
-;; Haml, Sass & Less
-(autoload 'haml-mode "haml-mode" "HAML mode" t)
-(autoload 'sass-mode "sass-mode" "SASS mode" t)
-;; (require 'sass-mode)
-;; (require 'haml-mode)
-
-(add-hook 'sass-mode-hook 
-  '(lambda () (add-hook 'local-write-file-hooks 
-    '(lambda () (save-excursion (untabify (point-min) (point-max)))))))
-
-(add-hook 'haml-mode-hook 
-  '(lambda () (add-hook 'local-write-file-hooks 
-    '(lambda () (save-excursion (untabify (point-min) (point-max)))))))
-
-(add-to-list 'auto-mode-alist '("\\.haml$"	. haml-mode))
-(add-to-list 'auto-mode-alist '("\\.scss$"	. sass-mode))
-(add-to-list 'auto-mode-alist '("\\.sass$"	. sass-mode))
-(add-to-list 'auto-mode-alist '("\\.less$"	. css-mode))
-
-;; Code Folding
-;; - http://www.emacswiki.org/emacs/FoldingMode
-(if (load "folding" 'nomessage 'noerror)
-    (folding-mode-add-find-file-hook))
-
-;; C-c only, not C-c @.
-(setq folding-default-keys-function
-      'folding-bind-backward-compatible-keys)
-
-;; Extra fold marks for nonstandard major modes
-(folding-add-to-marks-list
- 'sass-mode "// {{{ " "// }}}" nil t)
-
-;; Shortcuts
-(defalias 'eb						 'eval-buffer)
-(defalias 'er						 'eval-region)
-(defalias 'ee						 'eval-expression)
-(defalias 'fold		        			 'fold-enter-fold-mode-close-all-folds)
 
 (global-set-key (kbd "M--") 'hippie-expand)
 
+(setq scss-compile-at-save nil
+      scss-output-directory "../static/css")
+
+(add-hook 'scss-mode-hook 'flyspell-prog-mode)
 ;; Smart comments.
 ;; - http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
-(defun comment-dwim-line (&optional arg)
-  "Replacement for the comment-dwim command.
-	If no region is selected and current line is not blank and we are not at the end of the line,
-	then comment current line.
-	Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
-  (interactive "*P")
-  (comment-normalize-vars)
-  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
-      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
-    (comment-dwim arg)))
+ (defun comment-dwim-line (&optional arg)
+   "Replacement for the comment-dwim command.  If no region is
+    selected and current line is not blank and we are not at the end
+    of the line, then comment current line.  Replaces default
+    behaviour of comment-dwim, when it inserts comment at the end of
+    the line."
+   (interactive "*P")
+   (comment-normalize-vars)
+   (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+       (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+     (comment-dwim arg)))
 
-(global-set-key (kbd "M-;") 'comment-dwim-line)
-(global-set-key (kbd "C-c C-c") 'comment-dwim-line)
+ (global-set-key (kbd "M-;") 'comment-dwim-line)
+ (global-set-key (kbd "C-c C-c") 'comment-dwim-line)
 
 (defun unfill-region (begin end)
   "Remove all linebreaks in a region but leave paragraphs,
@@ -436,7 +284,11 @@
 (global-set-key (kbd "\C-xw") 'rename-current-file-or-buffer)
 
 ;; Indentation
-(require 'smart-tabs-mode)
+(autoload 'smart-tabs-mode "smart-tabs-mode"
+  "Intelligently indent with tabs, align with spaces!")
+(autoload 'smart-tabs-mode-enable "smart-tabs-mode")
+(autoload 'smart-tabs-advice "smart-tabs-mode")
+
 (setq-default indent-tabs-mode t)
 (setq tab-width 2)
 
@@ -472,21 +324,6 @@
 (add-hook 'ruby-mode-hook
 	  '(lambda()
 	     (local-set-key (kbd "RET") 'newline-and-indent)))
-
-;; Indent when pasting
-;; (dolist (command '(yank yank-pop))
-;;   (eval `(defadvice ,command (after indent-region activate)
-;; 	   (and (not current-prefix-arg)
-;; 		(member major-mode '(emacs-lisp-mode lisp-mode
-;; 						     clojure-mode		 scheme-mode
-;; 						     haskell-mode		 ruby-mode
-;; 						     rspec-mode			 python-mode
-;; 						     c-mode					 c++-mode
-;; 						     objc-mode			 latex-mode
-;; 						     plain-tex-mode	 sass-mode
-;; 						     haml-mode))
-;; 		(let ((mark-even-if-inactive transient-mark-mode))
-;; 		  (indent-region (region-beginning) (region-end) nil))))))
 
 ;; When killing the end of a line, eliminate any indentation of the next line
 (defadvice kill-line (before check-position activate)
@@ -552,24 +389,6 @@
   (yank)
   )
 (global-set-key (kbd "C-c d") 'duplicate-line)
-
-;; Yasnippet
-(require 'yasnippet)
-(yas/global-mode 0)
-
-;; Place my own snippet stuff in mysnippets
-(setq yas/snippet-dirs '("~/.emacs.d/snippets"
-			   "~/.emacs.d/vendor/yasnippet/snippets"))
-;; (mapc 'yas/load-directory yas/root-directory)
-(setq yas/prompt-functions '(yas/dropdown-prompt yas/ido-prompt yas/completing-prompt))
-
-;; Fix up for markdown-mode
-(add-hook 'markdown-mode-hook
-	  (let ((original-command (lookup-key org-mode-map [tab])))
-	    `(lambda ()
-	       (setq yas/fallback-behavior
-		     '(apply ,original-command))
-	       (local-set-key [tab] 'yas/expand))))
 
 (defun shutdown-emacs-server () (interactive)
   (when (not (eq window-system 'x))
