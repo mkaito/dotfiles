2142a89294c1b9ebf3359b080db5e688ecaecd5a
Michishige Kaito <me@mkaito.com>
2014-10-03 17:04:34 +0100

    Fix some transmission-remote shell functions

--- fish/functions/tt.fish
+++ fish/functions/tt.fish
@@ -2,8 +2,8 @@ function tt
 	set -l torrents
 	set -l tline
 
-	if not isatty 0
-		while read line
+	if not isatty stdin
+		while read -l line
 			# `grep` will only print the matching portion of lines that start with whitespace followed by digits.
 			# `tr` then removes any whitespace.
 			set torrents $torrents (echo $line | grep -Po '^\s*\d+' | tr -d ' ')
@@ -11,7 +11,7 @@ function tt
 
 		echo "Got torrents: $torrents."
 
-	# Makeshift hacky version of $torrents.join(',')
+		# Makeshift hacky version of $torrents.join(',')
 		set tline $torrents[1]
 		if test (count $torrents) -ge 2
 			for t in $torrents[2..-1]
